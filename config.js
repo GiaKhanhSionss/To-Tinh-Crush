6d38d9ff24a9ca228ac385104ae92696428c404d
GiaKhanhSionss <87819603+GiaKhanhSionss@users.noreply.github.com>
2021-07-22 21:46:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Phương à!',
+    introDesc: `Anh có điều muốn nói với em`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Trên thế giới này có 7 tỷ người người mà anh thích chỉ có em thôi ',
+    desc: 'Anh biết em cũng yêu anh nhiều lắm đúng hăm  ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question:'Có thể cho biết lý do vì sao thích được hăm ',
+    btnReply: 'Gửi cho béo <3',
+    reply: 'Vì anh đtrai lắmm',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Sau dịch anh dẫn bé đi chơi nhá ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/profile.php?id=100027020587975' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
